890840352ab918a5f990151d43fc195673f21d17
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2023-05-02 23:40:44 +0600

    Create Weather Observation Station 5 (mySQL).sql

---
+++ Weather Observation Station 5 (mySQL).sql
@@ -0,0 +1,14 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-5 --
+
+SELECT CITY,LENGTH(CITY)
+FROM STATION 
+ORDER BY LENGTH(CITY),CITY
+LIMIT 1;  
+
+
+SELECT CITY,LENGTH(CITY)
+FROM STATION 
+ORDER BY LENGTH(CITY) DESC,CITY
+LIMIT 1;  
+
+-- Success -- 
