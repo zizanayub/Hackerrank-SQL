8c7bb19d1bae0ba247d37b99c7b288827b6369fa
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2023-05-03 10:24:07 +0600

    Create Weather Observation Station 6.sql

---
+++ Weather Observation Station 6.sql
@@ -0,0 +1,8 @@
+-- https://www.hackerrank.com/challenges/weather-observation-station-6/ -- 
+
+
+SELECT City 
+FROM Station 
+WHERE City like 'a%' OR City like 'e%' OR City like 'i%' OR City like 'o%' OR City like 'u%'; 
+
+-- Success -- 
