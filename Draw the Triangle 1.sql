c1bafe5085519cb795d35be4d0b34b187d8fa8ad
Zizan Ayub Al-Khalil <65456659+zizanayub@users.noreply.github.com>
2023-08-07 11:06:47 +0600

    Update Draw the Triangle 1.sql

--- Draw the Triangle 1.sql
+++ Draw the Triangle 1.sql
@@ -1,4 +1,18 @@
+/*
+Question:
+P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
 
+Write a query to print the pattern P(20).
+*/
+
+
+
+-- First Solution
 WITH RECURSIVE stars AS
 (
 SELECT 
@@ -15,3 +29,42 @@ SELECT
 
 SELECT str FROM stars
 ORDER BY str DESC; 
+
+
+
+
+-- 2nd Solution
+
+
+set @num = 21;
+select repeat('* ',@num:=@num-1) from information_schema.tables limit 20;
+
+
+
+
+
+-- 3rd Solution
+DELIMITER //
+CREATE PROCEDURE Asterisk(n INT)
+BEGIN
+    DECLARE i INT DEFAULT 20;
+    WHILE i > 0 DO
+        SELECT REPEAT('* ', i);
+        SET i = i - 1;
+    END WHILE;
+END //
+DELIMITER ;
+
+CALL Asterisk(20)
+
+
+
+
+
+-- 4th Solution
+with recursive cte(Num) as
+(select 20 
+ union all
+ select num-1 from cte where num>1)
+ 
+ select repeat("* ",num) from cte;
